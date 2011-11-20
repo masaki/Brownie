1e67e82969ed4bfdf1de81a3d7111f9abf3f9357
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2011-11-21 03:07:11 +0900

    modify examples

--- t/lib/Test/Brownie/SharedExamples/Session.pm
+++ t/lib/Test/Brownie/SharedExamples/Session.pm
@@ -12,50 +12,47 @@ sub session_support_click_action {
     my ($session, $base_url) = @_;
 
     subtest 'click_link' => sub {
-        my $harness = sub {
-            my $locator = shift;
-            $session->visit($base_url);
-            $session->click_link($locator);
-            isnt $session->current_url => $base_url;
-        };
+        my @locator = (
+            'link_id',
+            'Text of Link',
+            'Title of Link',
+            'Alt of Image',
+        );
 
-        $harness->('link_id');
-        $harness->('Text of Link');
-        $harness->('Title of Link');
-        $harness->('Alt of Image');
+        for my $locator (@locator) {
+            $session->visit($base_url);
+            ok $session->click_link($locator);
+            isnt $session->current_url => $base_url;
+        }
     };
 
     subtest 'click_button' => sub {
-        my $harness = sub {
-            my $locator = shift;
+        my @locator = (
+            'input_submit',
+            'input_image',
+            'button_submit',
+            'Input Submit Title', 'Input Submit Value',
+            'Button Submit Title', 'Button Submit Value', 'Button Submit',
+        );
+
+        for my $locator (@locator) {
             $session->visit($base_url);
-            $session->click_button($locator);
-            isnt $session->current_url => $base_url;
-        };
-
-        my @id = qw(input_submit input_image button_submit);
-        for my $id (@id) {
-            $harness->($id);
+            ok $session->click_button($locator);
+            is $session->current_path => '/form';
         }
-
-        $harness->('Input Submit Title');
-        $harness->('Input Submit Value');
-
-        $harness->('Button Submit Title');
-        $harness->('Button Submit Value');
-        $harness->('Button Submit');
     };
 
     subtest 'click_on' => sub {
-        my $harness = sub {
-            my $locator = shift;
+        my @locator = (
+            'link_id',
+            'Input Submit Title',
+        );
+
+        for my $locator (@locator) {
             $session->visit($base_url);
             $session->click_on($locator);
             isnt $session->current_url => $base_url;
-        };
-
-        $harness->('link_id');
-        $harness->('Input Submit Title');
+        }
     };
 }
 
@@ -63,117 +60,137 @@ sub session_support_form_action {
     my ($session, $base_url) = @_;
 
     subtest 'fill_in' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['input_text', 'input_text'],
+            ['Input Text Title', 'input_text'],
+            ['Input Text Label', 'input_text'],
+            ['Inner Input Password Lavel', 'input_password'],
+            ['textarea', 'textarea'],
+            ['Textarea Label', 'textarea'],
+            ['Inner Textarea Lavel', 'textarea2'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
             my $value = time . $$;
-            $session->fill_in($locator, $value);
+            ok $session->fill_in($locator, $value);
             is $session->find_element("#$id")->value => $value;
-        };
-
-        $harness->('input_text', 'input_text');
-        $harness->('Input Text Title', 'input_text');
-        $harness->('Input Text Label', 'input_text');
-        $harness->('Inner Input Password Lavel', 'input_password');
-
-        $harness->('textarea', 'textarea');
-        $harness->('Textarea Label', 'textarea');
-        $harness->('Inner Textarea Lavel', 'textarea2');
+        }
     };
 
     subtest 'choose' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['input_radio1', 'input_radio1'],
+            ['Radio1 Value', 'input_radio1'],
+            ['radio1', 'input_radio1'],
+            ['radio3', 'input_radio3'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok $session->find_element("#$id")->is_not_selected;
-            $session->choose($locator);
-            ok $session->find_element("#$id")->is_selected;
-        };
-
-        $harness->('input_radio1', 'input_radio1');
-        $harness->('Radio1 Value', 'input_radio1');
-        $harness->('radio1', 'input_radio1');
-        $harness->('radio3', 'input_radio3');
+            my $node = $session->find_element("#$id");
+            ok $node->is_not_selected;
+            ok $session->choose($locator);
+            ok $node->is_selected;
+        }
     };
 
     subtest 'check' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['input_checkbox1', 'input_checkbox1'],
+            ['Checkbox1 Value', 'input_checkbox1'],
+            ['checkbox1', 'input_checkbox1'],
+            ['checkbox4', 'input_checkbox4'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok $session->find_element("#$id")->is_not_checked;
-            $session->check($locator);
-            ok $session->find_element("#$id")->is_checked;
-        };
-
-        $harness->('input_checkbox1', 'input_checkbox1');
-        $harness->('Checkbox1 Value', 'input_checkbox1');
-        $harness->('checkbox1', 'input_checkbox1');
-        $harness->('checkbox4', 'input_checkbox4');
+            my $node = $session->find_element("#$id");
+            ok $node->is_not_checked;
+            ok $session->check($locator);
+            ok $node->is_checked;
+        }
     };
 
     subtest 'uncheck' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['input_checkbox3', 'input_checkbox3'],
+            ['Checkbox3 Value', 'input_checkbox3'],
+            ['checkbox3', 'input_checkbox3'],
+            ['checkbox2', 'input_checkbox2'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok $session->find_element("#$id")->is_checked;
-            $session->uncheck($locator);
-            ok $session->find_element("#$id")->is_not_checked;
-        };
-
-        $harness->('input_checkbox3', 'input_checkbox3');
-        $harness->('Checkbox3 Value', 'input_checkbox3');
-        $harness->('checkbox3', 'input_checkbox3');
-        $harness->('checkbox2', 'input_checkbox2');
+            my $node = $session->find_element("#$id");
+            ok $node->is_checked;
+            ok $session->uncheck($locator);
+            ok $node->is_not_checked;
+        }
     };
 
     subtest 'select' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['select_option1', 'select_option1'],
+            ['3', 'select_option3'],
+            ['o4', 'select_option4'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok $session->find_element("#$id")->is_not_selected;
-            $session->select($locator);
-            ok $session->find_element("#$id")->is_selected;
-        };
-
-        $harness->('select_option1', 'select_option1');
-        $harness->('3', 'select_option3');
-        $harness->('o4', 'select_option4');
+            my $node = $session->find_element("#$id");
+            ok $node->is_not_selected;
+            ok $session->select($locator);
+            ok $node->is_selected;
+        }
     };
 
     subtest 'unselect' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['select_option5', 'select_option5'],
+            ['5', 'select_option5'],
+            ['o5', 'select_option5'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok $session->find_element("#$id")->is_selected;
-            $session->unselect($locator);
-            ok $session->find_element("#$id")->is_not_selected;
-        };
-
-        $harness->('select_option5', 'select_option5');
-        $harness->('5', 'select_option5');
-        $harness->('o5', 'select_option5');
+            my $node = $session->find_element("#$id");
+            ok $node->is_selected;
+            ok $session->select($locator);
+            ok $node->is_not_selected;
+        }
     };
 
     subtest 'attach_file' => sub {
-        my $harness = sub {
-            my ($locator, $id) = @_;
+        my @params = (
+            ['input_file1', 'input_file1'],
+            ['File Label1', 'input_file1'],
+            ['File Label2', 'input_file2'],
+        );
+
+        for (@params) {
+            my ($locator, $id) = @$_;
             $session->visit($base_url);
 
-            ok !$session->find_element("#$id")->value;
+            my $node = $session->find_element("#$id");
+            ok !($node->value);
+
             my $path = File::Spec->rel2abs($0);
             $session->attach_file($locator, $path);
-            is $session->find_element("#$id")->value => $path;
-        };
-
-        $harness->('input_file1', 'input_file1');
-        $harness->('File Label1', 'input_file1');
-        $harness->('File Label2', 'input_file2');
+            is $node->value => $path;
+        }
     };
 }
 
